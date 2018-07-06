4e926dc571d211be96f044cff86bc2451da36838
Maxim <maxrap7878@mail.ru>
2018-07-06 23:05:20 +0300

    final version

--- script.js
+++ script.js
@@ -17,6 +17,8 @@
 //         entriesCount: 21
 //     }
 // ];
+let __object_control;
+let __sessionNumber;
 let __appstate = "initial";
 let timer;
 let obj;
@@ -90,9 +92,9 @@ maxcount.addEventListener("focusout", ()=>{
         maxcount.value = "your field is empty";
         maxcount.style.color = "#c0392b";
         __validationRegistration.thirdInputIsValid = false;
-    }else if(maxcount.value< 1 || maxcount.value > 500){
+    }else if(maxcount.value< 1 || maxcount.value > 100){
         maxcount.style.border = "1px solid #c0392b";
-        maxcount.value = "value must be from 1 to 500";
+        maxcount.value = "value must be from 1 to 100";
         maxcount.style.color = "#c0392b";
         __validationRegistration.thirdInputIsValid = false;
     }else if(isNaN(maxcount.value)){
@@ -166,8 +168,8 @@ function validation() {
                 status.innerText = "paused";
                 status.style.color = "#fffa65";
                 status.style.borderColor = "#fffa65";
-
-                axios.post('http://localhost:8091/pause', "simpleSession")
+                console.log("session number " + __sessionNumber);
+                axios.post('http://localhost:8091/pause', __sessionNumber)
                     .then(function (response) {
                         console.log(response);
                     })
@@ -187,8 +189,9 @@ function validation() {
                 status.innerText = "stoped";
                 status.style.color = "#ff4d4d";
                 status.style.borderColor = "#ff4d4d";
+                console.log("session number " + __sessionNumber);
 
-                axios.post('http://localhost:8091/stop', "simpleSession")
+                axios.post('http://localhost:8091/stop', __sessionNumber)
                     .then(function (response) {
                         console.log(response);
                     })
@@ -207,18 +210,41 @@ function validation() {
 }
 start.addEventListener('click', Run);
 function Run(){
-    if(__appstate === "initial" || __appstate === "paused" || __appstate === "stopped"){
+    if(__appstate === "initial" || __appstate === "paused" || __appstate === "stopped") {
 
-        if(__appstate === "stopped"){
-            axios.post('http://localhost:8091/resume', "simpleSession")
+
+        if (__appstate === "initial") {
+            __sessionNumber = Math.random().toString(36).substring(7);
+            console.log("session number " + __sessionNumber);
+        }
+
+
+        if (__appstate === "stopped") {
+            obj = undefined;
+            __sessionNumber = Math.random().toString(36).substring(7);
+            console.log("session number after stop " + __sessionNumber);
+            axios.post('http://localhost:8091/resume', __sessionNumber)
                 .then(function (response) {
-                    console.log(response);
+                    // console.log(response);
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
+
+            let removeItems = document.getElementsByTagName("tr");
+            console.log(removeItems);
+            let arr = Array.prototype.slice.call(removeItems, 0);
+
+            arr.forEach(child => {
+                // let table = document.getElementById("table");
+                if (child.id !== "initial") {
+                    child.parentNode.removeChild(child);
+                }
+                // console.log(child);
+            });
+            tableRegistry = [{}];
         }
-        __appstate = "started";
+        // __appstate = "started";
 
 
         let status = document.getElementById("progress");
@@ -227,97 +253,132 @@ function Run(){
         status.style.borderColor = "lightgreen";
 
 
-        if(__validationRegistration.firstInputIsValid && __validationRegistration.secondInputIsValid && __validationRegistration.thirdInputIsValid && __validationRegistration.fourthInputIsValid) {
+        if (__validationRegistration.firstInputIsValid && __validationRegistration.secondInputIsValid && __validationRegistration.thirdInputIsValid && __validationRegistration.fourthInputIsValid) {
             console.log("run");
-            __appstate = "started";
+            if (__appstate !== "paused") {
+                axios.post('http://localhost:8091/openNewSession', JSON.stringify({
+                    url: ulrinput.value,
+                    symbol: symbols.value,
+                    maxCount: maxcount.value,
+                    maxUrl: maxurl.value,
+                    sessionNumber: __sessionNumber
 
-            // let check =  {
-            //          url: ulrinput.value,
-            //          symbol: symbols.value,
-            //          maxCount: maxcount.value,
-            //          maxUrl: maxurl.value
-            //   };
-
-            axios.post('http://localhost:8091/openNewSession', JSON.stringify({
-                url: ulrinput.value,
-                symbol: symbols.value,
-                maxCount: maxcount.value,
-                maxUrl: maxurl.value
-            }),)
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-
-            timer = setInterval( ()=>{
-                axios.get('http://localhost:8091/getUpdatedUrl')
+                }))
                     .then(function (response) {
-                        console.log(response);
-                        obj = response.data;
-                        console.log(obj);
-                        return obj
                     })
-                    .then( ()=> {
-                        obj.forEach((element)=>{
 
-                            let checker = tableRegistry.some((writen)=>{
-                                return writen.id === element.id
-                            });
-
-                            if(!checker){
-                                tableRegistry.push(element);
-                                let table = document.getElementById("table");
-                                let mainTR = document.createElement("tr");
-                                mainTR.id = element.id;
-                                let numberTD = document.createElement("td");
-                                numberTD.innerText = element.id;
-                                let urlTD = document.createElement("td");
-                                urlTD.innerText = element.url;
-                                let statusTD = document.createElement("td");
-                                statusTD.innerText = element.status;
-                                let foundTD = document.createElement("td");
-                                foundTD.innerText = element.entriesCount;
-
-                                table.appendChild(mainTR);
-                                mainTR.appendChild(numberTD);
-                                mainTR.appendChild(urlTD);
-                                mainTR.appendChild(statusTD);
-                                mainTR.appendChild(foundTD);
-                            }
-
-                            tableRegistry.forEach((updatel)=>{
-                                if(element.id === updatel.id && element.status !== updatel.status){
-
-                                    let changeElement = document.getElementById(element.id).childNodes;
-                                    changeElement[2].innerHTML = element.status;
-                                    changeElement[3].innerHTML = element.entriesCount;
-                                    console.log(changeElement);
-                                    console.log(changeElement[2]);
-                                    console.log(changeElement[3]);
-                                    console.log(element.status);
-                                    console.log(element.status);
-
-
-
-                                }
-
-                            });
-
-                        })}
-                    )
                     .catch(function (error) {
                         console.log(error);
                     });
+            }else{
+                axios.post('http://localhost:8091/resume', __sessionNumber)
+                    .then(function (response) {
+                        // console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+            }
+            __appstate = "started";
+            timer = setInterval(() => {
+                console.log("OC = " + obj === []);
+
+                if( obj !== undefined && obj.length === 0){
+
+
+                    __appstate = "stopped";
+                    clearInterval(timer);
+
+                    let status = document.getElementById("progress");
+                    status.innerText = "stoped";
+                    status.style.color = "#ff4d4d";
+                    status.style.borderColor = "#ff4d4d";
+                    console.log("session number " + __sessionNumber);
+
+                    axios.post('http://localhost:8091/stop', __sessionNumber)
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                }else{
+
+                axios.get('http://localhost:8091/getUpdatedUrl', {
+                params:{
+                    sessionNumber:__sessionNumber
+                }
+                })
+                    .then(function (response) {
+                        console.log(response);
+                        obj = response.data;
+                        return obj
+                    })
+                    .then(() => {
+                        console.log("in function obj" + obj);
+
+                            obj.forEach((element) => {
+
+                                let checker = tableRegistry.some((writen) => {
+                                    return writen.id === element.id
+                                });
+
+                                if (!checker) {
+                                    tableRegistry.push(element);
+                                    let table = document.getElementById("table");
+                                    let mainTR = document.createElement("tr");
+                                    mainTR.id = element.id;
+                                    let numberTD = document.createElement("td");
+                                    numberTD.innerText = element.id;
+                                    let urlTD = document.createElement("td");
+                                    urlTD.innerText = element.url;
+                                    let statusTD = document.createElement("td");
+                                    statusTD.innerText = element.status;
+                                    let foundTD = document.createElement("td");
+                                    foundTD.innerText = element.entriesCount;
+
+                                    table.appendChild(mainTR);
+                                    mainTR.appendChild(numberTD);
+                                    mainTR.appendChild(urlTD);
+                                    mainTR.appendChild(statusTD);
+                                    mainTR.appendChild(foundTD);
+                                }
+
+                                tableRegistry.forEach((updatel) => {
+                                    if (element.id === updatel.id && element.status !== updatel.status) {
+
+                                        let changeElement = document.getElementById(element.id).childNodes;
+                                        changeElement[2].innerHTML = element.status;
+                                        changeElement[3].innerHTML = element.entriesCount;
+                                        console.log(changeElement);
+                                        console.log(changeElement[2]);
+                                        console.log(changeElement[3]);
+                                        console.log(element.status);
+                                        console.log(element.status);
+
+
+                                    }
+
+                                });
+
+                            })
+                        }
+
+                    )
+
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                }
                 console.log("updated");
 
             }, 1000);
 
 
         }
-    }
 
+    }
 
 }
 
